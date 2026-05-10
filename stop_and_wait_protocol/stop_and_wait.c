fbd239d6f9d6248d9f190b1aeb5cef98cec89272
Anasmita-Math <mathanasmita@gmail.com>
2026-05-11 01:56:17 +0530

    new1

--- stop_and_wait_protocol/stop_and_wait.c
+++ stop_and_wait_protocol/stop_and_wait.c
@@ -2,33 +2,38 @@
 #include <stdlib.h>
 
 int main() {
-    int i, j, noframes, x, x1 = 10, x2;
-    for (i = 0; i < 200; i++) {
+
+    int i, noframes, x;
+
+    for(i = 0; i < 200; i++) {
         rand();
     }
+
     noframes = rand() % 200;
-    i = 1;
-    j = 1;
+
     noframes = noframes / 8;
 
-    printf("\n number of frames is %d \n", noframes);
-    while (noframes > 0) {
-        printf("\n sending frame %d \n", i);
-        srand(x1++);
-        x = rand() % 10;
-        if (x % 2 == 0) {
-            for (x2 = 1; x2 < 2; x2++) {
-                printf("\n waiting for %d second \n", x2);
-            }
-            printf("\n sending frame %d \n", i);
-            srand(x1++);
-            x = rand() % 10;
+    printf("\nNumber of frames : %d\n", noframes);
+
+    for(i = 1; i <= noframes; i++) {
+
+        printf("\nSending Frame %d", i);
+
+        x = rand() % 2;
+
+        if(x == 0) {
+
+            printf("\nAcknowledgement Lost");
+
+            printf("\nResending Frame %d", i);
+
+            printf("\nWaiting for ACK...\n");
         }
-        printf("\n Ack for frames %d ", j);
-        noframes -= 1;
-        i++;
-        j++;
+
+        printf("\nAcknowledgement received for Frame %d\n", i);
     }
-    printf("\n end of stop and wait protocall \n");
+
+    printf("\nStop and Wait Protocol Completed\n");
+
     return 0;
 }
